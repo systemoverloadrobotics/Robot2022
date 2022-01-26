14869417d4934cbbf9881c634b4a5de67c93a106
OneLemonyBoi <50998649+OneLemonyBoi@users.noreply.github.com>
2022-01-25 22:46:24 -0800

    Shooter Command

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -1,10 +1,12 @@
 package frc.robot.commands;
 
+import org.opencv.core.Mat;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Limelight.LimelightTable;
 
 public class ShooterCommand extends CommandBase {
 	private Limelight limelight; 
@@ -25,12 +27,17 @@ public class ShooterCommand extends CommandBase {
 	 // Called at 50hz while the command is scheduled.
 	 @Override
 	 public void execute() {
-		 
+		 // Checks if aimed
+		 if (Math.abs(limelight.valueSupplier(LimelightTable.TX, Double.class).get()) < 0.5) {
+			 shooter.shoot(0.5);
+		 }
 	 }
  
 	 // Called once when the command ends or is interrupted.
 	 @Override
-	 public void end(boolean interrupted) {}
+	 public void end(boolean interrupted) {
+		 shooter.stopMotor();
+	 }
  
 	 @Override
 	 public boolean isFinished() {
