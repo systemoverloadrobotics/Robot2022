8b2c5db7d3c4edd2ea413b355d937a1b37268b06
OneLemonyBoi <50998649+OneLemonyBoi@users.noreply.github.com>
2022-02-08 20:22:06 -0800

    Replace Falcon 500 with NEO

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,17 +3,21 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
-	private WPI_TalonFX shooterMotor;
+	private CANSparkMax shooterMotor;
 
 	public Shooter() {
-		shooterMotor = new WPI_TalonFX(Constants.Motor.SHOOTER_PORT);
-		shooterMotor.config_kP(0, Constants.PID.SHOOTER_P);
-		shooterMotor.config_kI(0, Constants.PID.SHOOTER_I);
-		shooterMotor.config_kD(0, Constants.PID.SHOOTER_D);
+		shooterMotor = new CANSparkMax(Constants.Motor.SHOOTER_PORT, MotorType.kBrushless);
+		shooterMotor.getPIDController().setP(Constants.PID.SHOOTER_P);
+		shooterMotor.getPIDController().setI(Constants.PID.SHOOTER_I);
+		shooterMotor.getPIDController().setD(Constants.PID.SHOOTER_D);
 	}
 
 	public int shooterMotorRPM() {
@@ -22,10 +26,10 @@ public class Shooter extends SubsystemBase {
 	}
 
 	public void spool(double rpm) {
-		shooterMotor.set(ControlMode.PercentOutput, rpm / 6380D);
+		shooterMotor.set(rpm / 5676D);
 	}
 
 	public void stopMotor() {
-		shooterMotor.set(ControlMode.PercentOutput, 0);
+		shooterMotor.set(0);
 	}
 }
