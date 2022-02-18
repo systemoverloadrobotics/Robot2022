35dd1545a9513cbe9ad4a2b14ff8429b4af085f7
Yash Chhatre <ychhatre@gmail.com>
2022-02-18 08:57:06 -0800

    fixed swerve autopaths

--- src/main/java/frc/robot/commands/auto/AutoPaths.java
+++ src/main/java/frc/robot/commands/auto/AutoPaths.java
@@ -27,95 +27,92 @@ import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.Storage.ToggleState;
 
 public class AutoPaths {
-  /* Sample Auto for Swerve */
-  private static Swerve swerve;
-  private static Intake intake;
-  private static Storage storage;
-  private static Shooter shooter;
-  private static Limelight limelight; 
+    /* Sample Auto for Swerve */
+    private static Swerve swerve;
+    private static Intake intake;
+    private static Storage storage;
+    private static Shooter shooter;
+    private static Limelight limelight;
 
 
 
-  public AutoPaths(Swerve swerve, Intake intake, Storage storage, Shooter shooter, Limelight limelight) {
-    this.swerve = swerve;
-    this.intake = intake;
-    this.storage = storage;
-    this.shooter = shooter;
-    this.limelight = limelight; 
-  }
+    public AutoPaths(Swerve swerve, Intake intake, Storage storage, Shooter shooter,
+            Limelight limelight) {
+        this.swerve = swerve;
+        this.intake = intake;
+        this.storage = storage;
+        this.shooter = shooter;
+        this.limelight = limelight;
+    }
 
-  private static SwerveControllerCommand moveDriveTrain(int startPoint, int endPoint,
-      int rotation) {
-    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(Constants.Motor.SWERVE_MAX_SPEED,
-        Constants.Motor.SWERVE_MAX_ACCELERATION);
+    private static SwerveControllerCommand moveDriveTrain(int startPoint, int endPoint,
+            int rotation) {
+        TrajectoryConfig trajectoryConfig = new TrajectoryConfig(Constants.Motor.SWERVE_MAX_SPEED,
+                Constants.Motor.SWERVE_MAX_ACCELERATION);
 
-    Trajectory trajectory =
-        TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)),
-            List.of(new Translation2d(0, startPoint), new Translation2d(0, endPoint)),
-            new Pose2d(0, endPoint, Rotation2d.fromDegrees(rotation)), trajectoryConfig);
+        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+                new Pose2d(0, 0, new Rotation2d(0)),
+                List.of(new Translation2d(0, startPoint), new Translation2d(0, endPoint)),
+                new Pose2d(0, endPoint, Rotation2d.fromDegrees(rotation)), trajectoryConfig);
 
-    PIDController xController = new PIDController(Constants.PID.P_X_CONTROLLER, 0, 0);
-    PIDController yController = new PIDController(Constants.PID.P_Y_CONTROLLER, 0, 0);
-    ProfiledPIDController thetaController = new ProfiledPIDController(
-        Constants.PID.P_THETA_CONTROLLER, 0, 0, Constants.Motor.THETA_CONTROL_CONSTRAINTS);
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+        PIDController xController = new PIDController(Constants.PID.P_X_CONTROLLER, 0, 0);
+        PIDController yController = new PIDController(Constants.PID.P_Y_CONTROLLER, 0, 0);
+        ProfiledPIDController thetaController = new ProfiledPIDController(
+                Constants.PID.P_THETA_CONTROLLER, 0, 0, Constants.Motor.THETA_CONTROL_CONSTRAINTS);
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
-    return new SwerveControllerCommand(trajectory, swerve::getPose,
-        Constants.Motor.SWERVE_DRIVE_KINEMATICS, xController, yController, thetaController,
-        swerve::setModuleStates, swerve);
-  }
-
-
-  public static Command twoBallAuto() {
-    // move backwards
+        return new SwerveControllerCommand(trajectory, swerve::getPose,
+                Constants.Motor.SWERVE_DRIVE_KINEMATICS, xController, yController, thetaController,
+                swerve::setModuleStates, swerve);
+    }
 
 
+    public static Command twoBallAuto() {
+        SwerveControllerCommand initialMovementCommand = moveDriveTrain(1, 7, 0);
 
-    SwerveControllerCommand initialMovementCommand = moveDriveTrain(1, 7, 0);
-
-    SwerveControllerCommand moveToShoot = moveDriveTrain(1, 9, 180);
-
-
-    IntakeBall intakeCommand = new IntakeBall(intake);
-    ShooterCommand shooterCommand = new ShooterCommand(limelight, shooter);
-    StorageCommand storageCommand = new StorageCommand(storage);
-
-    return new SequentialCommandGroup(
-        new InstantCommand(() -> swerve.resetOdometry(new Pose2d(0, 0, new Rotation2d(0)))),
-        new ParallelCommandGroup(initialMovementCommand, intakeCommand, storageCommand),
-        new InstantCommand(() -> swerve.resetOdometry(new Pose2d(0, 0, new Rotation2d(0)))),
-        moveToShoot, shooterCommand);
-  }
-
-  public static Command exampleAuto() {
-    // create trajectory settings
-    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(Constants.Motor.SWERVE_MAX_SPEED,
-        Constants.Motor.SWERVE_MAX_ACCELERATION)
-            .setKinematics(Constants.Motor.SWERVE_DRIVE_KINEMATICS);
-
-    // generate trajectory
-    Trajectory trajectory =
-        TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)),
-            List.of(new Translation2d(1, 0), new Translation2d(1, -1)),
-            new Pose2d(2, -1, Rotation2d.fromDegrees(180)), trajectoryConfig);
-
-    // pid controllers for tracking trajectory
-    PIDController xController = new PIDController(Constants.PID.P_X_CONTROLLER, 0, 0);
-    PIDController yController = new PIDController(Constants.PID.P_Y_CONTROLLER, 0, 0);
-    ProfiledPIDController thetaController = new ProfiledPIDController(
-        Constants.PID.P_THETA_CONTROLLER, 0, 0, Constants.Motor.THETA_CONTROL_CONSTRAINTS);
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
-
-    // construct command to follow trajectory
-    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(trajectory,
-        swerve::getPose, Constants.Motor.SWERVE_DRIVE_KINEMATICS, xController, yController,
-        thetaController, swerve::setModuleStates, swerve);
-
-    return new SequentialCommandGroup(
-        new InstantCommand(() -> swerve.resetOdometry(trajectory.getInitialPose())),
-        swerveControllerCommand, new InstantCommand(() -> swerve.stopModules()));
+        SwerveControllerCommand moveToShoot = moveDriveTrain(1, 9, 0);
 
 
-  }
+        IntakeBall intakeCommand = new IntakeBall(intake);
+        ShooterCommand shooterCommand = new ShooterCommand(limelight, shooter);
+        StorageCommand storageCommand = new StorageCommand(storage);
+
+        return new SequentialCommandGroup(
+                new InstantCommand(() -> swerve.resetOdometry(new Pose2d(0, 0, new Rotation2d(0)))),
+                new ParallelCommandGroup(initialMovementCommand, intakeCommand, storageCommand),
+                new InstantCommand(() -> swerve.resetOdometry(new Pose2d(0, 0, new Rotation2d(0)))),
+                moveToShoot, shooterCommand);
+    }
+
+    public static Command exampleAuto() {
+        // create trajectory settings
+        TrajectoryConfig trajectoryConfig = new TrajectoryConfig(Constants.Motor.SWERVE_MAX_SPEED,
+                Constants.Motor.SWERVE_MAX_ACCELERATION)
+                        .setKinematics(Constants.Motor.SWERVE_DRIVE_KINEMATICS);
+
+        // generate trajectory
+        Trajectory trajectory =
+                TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)),
+                        List.of(new Translation2d(1, 0), new Translation2d(1, -1)),
+                        new Pose2d(2, -1, Rotation2d.fromDegrees(180)), trajectoryConfig);
+
+        // pid controllers for tracking trajectory
+        PIDController xController = new PIDController(Constants.PID.P_X_CONTROLLER, 0, 0);
+        PIDController yController = new PIDController(Constants.PID.P_Y_CONTROLLER, 0, 0);
+        ProfiledPIDController thetaController = new ProfiledPIDController(
+                Constants.PID.P_THETA_CONTROLLER, 0, 0, Constants.Motor.THETA_CONTROL_CONSTRAINTS);
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+        // construct command to follow trajectory
+        SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(trajectory,
+                swerve::getPose, Constants.Motor.SWERVE_DRIVE_KINEMATICS, xController, yController,
+                thetaController, swerve::setModuleStates, swerve);
+
+        return new SequentialCommandGroup(
+                new InstantCommand(() -> swerve.resetOdometry(trajectory.getInitialPose())),
+                swerveControllerCommand, new InstantCommand(() -> swerve.stopModules()));
+
+
+    }
 
 }
